ab047ca113d091dfed92c58a963aa23a3e28e79b
Luma <ach.lumap@gmail.com>
2023-10-03 08:57:12 +0100

    T347255
    Signed-off-by: Luma <ach.lumap@gmail.com>

---
+++ bugs/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="home"),
+    path('register_bug/', views.register_bug, name='register_bug'),
+    path('bug/<int:bug_id>/', views.view_bug, name='view_bug'),
+    path('bug_list/', views.bug_list, name='bug_list'),
+]
--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Bug
+from .forms import BugForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
+def register_bug(request):
+    if request.method == 'POST':
+        form = BugForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bug_list')  
+    else:
+        form = BugForm()
+    return render(request, 'register_bug.html', {'form': form})
+
+def view_bug(request, bug_id):
+    bug = Bug.objects.get(id=bug_id)
+    return render(request, 'view_bug.html', {'bug': bug})
+
+def bug_list(request):
+    bugs = Bug.objects.all()
+    return render(request, 'bug_list.html', {'bugs': bugs})
